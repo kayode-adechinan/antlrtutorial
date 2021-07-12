d3e37596f5f510f4d0169411e49b158150b94801
Kayode Adechinan <kayode.adechinan@gmail.com>
2021-07-12 12:41:56 +0000

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+https://www.javahelps.com/2019/04/antlr-hello-world-arithmetic-expression.html
+
+
+mvn install
+
+mvn package
---
+++ src/test/java/Main.java
@@ -0,0 +1,15 @@
+import org.antlr.v4.runtime.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class Main {
+    @Test
+    void test_Add() {
+        assertThat("abc").isEqualTo("abc");
+    }
+
+}
